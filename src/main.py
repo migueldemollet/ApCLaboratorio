fdad4ddb7c81347e2f308b9fe5db5959d0a6cb1a
Miguel del Arco <migueldemollet10@gmail.com>
2021-10-14 21:36:23 +0200

    Mostrando las tablas

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import scipy.stats
+import seaborn as sns
+
 
 # Visualitzarem només 3 decimals per mostra
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
@@ -22,3 +24,36 @@ y = data[:, 2]
 print("Dimensionalitat de la BBDD:", dataset.shape)
 print("Dimensionalitat de les entrades X", x.shape)
 print("Dimensionalitat de l'atribut Y", y.shape)
+
+print("Per comptar el nombre de valors no existents:")
+print(dataset.isnull().sum())
+
+print("Per visualitzar les primeres 5 mostres de la BBDD:")
+print(dataset.head())
+
+print("Per veure estadístiques dels atributs numèrics de la BBDD:")
+print(dataset.describe())
+
+# mostrem atribut 0
+plt.figure()
+ax = plt.scatter(x[:,0], y)
+plt.show()
+
+plt.figure()
+plt.title("Histograma de l'atribut 0")
+plt.xlabel("Attribute Value")
+plt.ylabel("Count")
+hist = plt.hist(x[:,0], bins=11, range=[np.min(x[:,0]), np.max(x[:,0])], histtype="bar", rwidth=0.8)
+plt.show()
+
+# Mirem la correlació entre els atributs d'entrada per entendre millor les dades
+correlacio = dataset.corr()
+
+plt.figure()
+
+ax = sns.heatmap(correlacio, annot=True, linewidths=.5)
+plt.show()
+
+# Mirem la relació entre atributs utilitzant la funció pairplot
+relacio = sns.pairplot(dataset)
+plt.show()
