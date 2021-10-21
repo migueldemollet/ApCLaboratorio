c16b135def8d3370e81e88fb9eac1f546ffb26ca
Miguel del Arco <migueldemollet10@gmail.com>
2021-10-21 20:27:06 +0200

    Apartado C

--- src/main.py
+++ src/main.py
@@ -1,59 +1,14 @@
 from sklearn.datasets import make_regression
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 import scipy.stats
 import seaborn as sns
 
 
-# Visualitzarem només 3 decimals per mostra
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-
-# Funcio per a llegir dades en format csv
-def load_dataset(path):
-    dataset = pd.read_csv(path, header=0, delimiter=',')
-    return dataset
-
-# Carreguem dataset d'exemple
-dataset = load_dataset('db\\test_data.csv')
-data = dataset.values
-
-x = data[:, :2]
-y = data[:, 2]
-
-print("Dimensionalitat de la BBDD:", dataset.shape)
-print("Dimensionalitat de les entrades X", x.shape)
-print("Dimensionalitat de l'atribut Y", y.shape)
-
-print("Per comptar el nombre de valors no existents:")
-print(dataset.isnull().sum())
-
-print("Per visualitzar les primeres 5 mostres de la BBDD:")
-print(dataset.head())
-
-print("Per veure estadístiques dels atributs numèrics de la BBDD:")
-print(dataset.describe())
-
-# mostrem atribut 0
-plt.figure()
-ax = plt.scatter(x[:,0], y)
-plt.show()
+x = [1,2,3,4,5,6,7,1,1,1,1,1]
+y = [1,2,3,5,6,7,5,5,5,5,55,5]
 
 plt.figure()
-plt.title("Histograma de l'atribut 0")
-plt.xlabel("Attribute Value")
-plt.ylabel("Count")
-hist = plt.hist(x[:,0], bins=11, range=[np.min(x[:,0]), np.max(x[:,0])], histtype="bar", rwidth=0.8)
-plt.show()
-
-# Mirem la correlació entre els atributs d'entrada per entendre millor les dades
-correlacio = dataset.corr()
-
-plt.figure()
-
-ax = sns.heatmap(correlacio, annot=True, linewidths=.5)
-plt.show()
-
-# Mirem la relació entre atributs utilitzant la funció pairplot
-relacio = sns.pairplot(dataset)
+plt.bar(x,y)
 plt.show()
